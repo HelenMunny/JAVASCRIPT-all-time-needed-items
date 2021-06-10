267ac1b1980308b3711b5bc8536744639860ef5b
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2021-06-10 03:54:31 +0300

    final corrections

--- RESPONSIVE-NAVBAR/app.js
+++ RESPONSIVE-NAVBAR/app.js
@@ -10,10 +10,13 @@ navToggle.addEventListener('click', function () {
  if (!navExpand) {
   navLinks.classList.add('show');
   nav.style.height = 'auto';
+  navToggle.innerHTML = '<i class="fa fa-2x fa-times" aria-hidden="true"></i>';
+  navToggle.style.color='rgb(59, 32, 133)'
   navExpand = true;
  } else {
   navLinks.classList.remove('show');
   nav.style.height = '80px';
+  navToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
   navExpand = false;
  }
 });
