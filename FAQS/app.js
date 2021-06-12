acf70cdc40c3a2b6d632564869f6368d1a0bce41
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2021-06-12 23:35:56 +0300

    styling and positioning except js

---
+++ FAQS/app.js
@@ -0,0 +1,5 @@
+let expandBtn = document.querySelectorAll(".qsn-expand");
+let collapseBtn = document.querySelectorAll(".qsn-collapse")
+let qsnAns = document.querySelectorAll(".qsn-ans");
+ 
+console.log(expandBtn);
