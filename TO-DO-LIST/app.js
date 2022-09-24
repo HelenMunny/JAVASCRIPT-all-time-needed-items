5ed0a277eb9172b16f4269fee0c7ed60089d5e0d
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-09-23 22:52:44 -0500

    problems finishing project

--- TO-DO-LIST/app.js
+++ TO-DO-LIST/app.js
@@ -1,1 +1,23 @@
-console.log("hi");
+window.addEventListener('load', function () {
+ const form = document.querySelector('form');
+const task = document.getElementById('newTask');
+const addBtn = document.querySelector('.addTasks');
+
+form.addEventListener('submit', function (e) {
+ e.preventDefault();
+ 
+ const taskInput = task.ariaValueMax;
+ if (!taskInput) {
+  alert('please! add a task!');
+  return
+ } 
+})
+
+// addBtn.addEventListener("click", function() {
+//  console.log(taskInput);
+// });
+
+})
+
+
+
