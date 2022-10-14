b52597d7eaf818298a47c33542404753ea82e777
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-10-13 22:30:38 -0500

    grocery list in progress

--- GROCERY-BUD/app.js
+++ GROCERY-BUD/app.js
@@ -11,5 +11,71 @@ const clearBtn = document.querySelector(".clearBtn");
 
 // edit option
 let editElement;
-let editFlag = fasle;
+let edit = false;
 let editID = "";
+
+// event listeners
+// submit form
+form.addEventListener("submit", function (e) {
+ e.preventDefault();
+ const value = input.value;
+ const id = new Date().getTime().toString();
+
+ if (value && !edit) {
+  const element = document.createElement('article');
+  element.classList.add('groceryItem');
+  // add id dynamically
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+
+
+  element.setAttributeNode(attr);
+  element.innerHTML = ` <p class="itemName">${value}</p>
+  <div class="btnContainer">
+   <button type="button" class="editBtn"><i class="fas fa-edit"></i></button>
+   <button type="button" class="deleteBtn"><i class="fas fa-trash"></i></button>
+  </div> `;
+  
+  // append child
+  list.appendChild(element);
+
+  // display alert
+  displayAlert('Item successfully added to the list!', 'green')
+
+  // show container
+  groceryContainer.classList.add('show');
+
+  // add to local storage
+  addToLocalStorage(id, value);
+  // set back to default
+  setBackToDefault();
+  }
+ else if (value && edit) {
+  console.log('editing the item');
+ }
+ else if (!value){
+  displayAlert('Please Enter an item!', 'red');
+ }
+})
+
+// display alert
+function displayAlert(text, color) {
+ alert.innerHTML = text;
+ alert.style.color = color;
+ 
+ // remove alert after sometime
+ setTimeout(function () {
+  alert.innerHTML = '';
+ },2000)
+}
+
+
+// add to local storage
+function addToLocalStorage(id, value) {
+ console.log('added to local storage');
+}
+
+// set back to default
+function setBackToDefault() {
+ console.log('set back to default');
+}
