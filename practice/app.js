f6031d65ca31f7a15a9fce03a9d46a98e1b7d17f
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-10-28 23:37:53 -0500

    call apply arguments bind

--- practice/app.js
+++ practice/app.js
@@ -1,11 +1,11 @@
 
 // constructor function
-function Person(firstName, lastName) {
- this.prothomNaam = firstName;
- this.seshNaam = lastName;
- this.fullName = function () {
-  console.log(`My name is ${this.prothomNaam} ${this.seshNaam} and I love react`);
- }
-}
-const john = new Person('John', 'Smilga');
-john.fullName();
+// function Person(firstName, lastName) {
+//  this.prothomNaam = firstName;
+//  this.seshNaam = lastName;
+//  this.fullName = function () {
+//   console.log(`My name is ${this.prothomNaam} ${this.seshNaam} and I love react`);
+//  }
+// }
+// const john = new Person('John', 'Smilga');
+// john.fullName();
