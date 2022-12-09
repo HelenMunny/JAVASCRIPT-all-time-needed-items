fd1992bd7a560f8e9f4108ade9f6bf61b53faa07
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-12-09 00:21:09 -0600

    set, objects

--- practice/app.js
+++ practice/app.js
@@ -1,11 +1,30 @@
 
+// Object.keys(), Object.values (), Object.entries()
+const person = {
+ name: 'john',
+ age: 25,
+ status: 'married',
+ profession: 'developer',
+}
+const keys = Object.keys(person);
+console.log(keys)
+const values = Object.values(person);
+console.log(values)
+const entries = Object.entries(person);
+console.log(entries)
+const newEntries = entries.map(entry => {
+ const [first, second] = entry;
+ console.log(first, second);
+})
 
+// set objects
+const unique = new Set();
+const third = 'third';
 
-
-// rest operators. when we use rest operators for destructuring, the position matters. we cannot place rest in the front place. because rest operator always return rest of the items. also we can not use ...rest keyword again and again. we have to use different keywords everytime.
-
-const numbers = [2, 7, 9, 11, 33, 45];
-const [two, , nine, eleven, ...rest] = numbers;
-console.log (two, nine, eleven, rest)
-
-
+unique.add('first');
+unique.add('second');
+unique.add(third);
+unique.add('first');  // only unique value would be added. first will be added just for once
+unique.delete('first');
+unique.has('first');   //false
+unique.clear()   //clears/deletes all items
