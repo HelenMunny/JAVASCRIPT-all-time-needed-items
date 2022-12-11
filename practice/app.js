94863d403d813bb86b2f8182f6f63b6b75899caf
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-12-10 22:22:50 -0600

    modules es6

--- practice/app.js
+++ practice/app.js
@@ -1,29 +1,21 @@
 
-// Object.keys(), Object.values (), Object.entries()
-const person = {
- name: 'john',
- age: 25,
- status: 'married',
- profession: 'developer',
-}
-const keys = Object.keys(person);
-console.log(keys)
-const values = Object.values(person);
-console.log(values)
-const entries = Object.entries(person);
-console.log(entries)
-const newEntries = entries.map(entry => {
- const [first, second] = entry;
- console.log(first, second);
-})
 
-// set objects
-const unique = new Set();
-const third = 'third';
-unique.add('first');
-unique.add('second');
-unique.add(third);
-unique.add('first');  // only unique value would be added. first will be added just for once
-unique.delete('first');
-unique.has('first');   //false
-unique.clear()   //clears/deletes all items
+const people = [
+ { name: 'Helen', job: 'Developer' },
+ { name: 'Tony', job: 'Designer' },
+ {name:'Monika', job:'Manager'}
+]
+const cont = document.querySelector('.conatiner');
+const btn = document.querySelector('.btn');
+
+const showPeople = () => {
+ const newPeople= people.map((person) => {
+  const {name,job} = person;
+  return `<p> ${name}, <strong> ${job} </strong> </p>`;
+ }).join('');
+ cont.textContent = 'Hello';
+};
+
+btn.addEventListener('click', () => {
+ showPeople();
+});
