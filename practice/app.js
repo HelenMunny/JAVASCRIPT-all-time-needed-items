44285a147474f376285f540e1e13209a35969329
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-12-16 07:55:43 -0600

    async js

--- practice/app.js
+++ practice/app.js
@@ -1,27 +1,49 @@
-// import {people,container,btn} from './utilities/utils.js'
-// const showPeople = () => {
-//  const newPeople = people.map((person) => {
-//   const { name, job } = person;
-//   return `<p>${name}, <strong> ${job} </strong> </p>`
-//  }).join('');
-//  container.innerHTML = newPeople;
-// };
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const btn = document.querySelector('.btn');
+const container = document.querySelector('.img-container');
+const url = "https://source.unsplash.com/random";
 
-// btn.addEventListener('click', () => {
-//  showPeople();
-// });
+btn.addEventListener('click', () => {
+ loadImage(url)
+  .then((img) => { container.appendChild(img) })
+  .catch((err) => {
+   console.log(err);
+  })
+ 
+ changeColor(red, 'red', 1000)
+  .then(() => { changeColor(green, 'green', 1000) })
+  .then(() => { changeColor(blue, 'blue', 2000) })
+ .catch((err) => {
+  console.log(err);
+ })
+})
 
-function boilWater() {
- console.log('boiling water.....')
- setTimeout(() => {
-  setTimeout(() => {
-   console.log('chopping carrots');
-  },2000)
-  console.log('boiling water is done');
- },5000)
+function loadImage(url) {
+ const promise = new Promise((resolve,reject) => {
+  let img = new Image();
+  img.src = url;
+  img.addEventListener('load', () => {
+   resolve(img);
+  })
+  img.addEventListener('error', () => {
+   reject(new Error('failed to load image from the url'));
+  })
+ })
+ return promise;
 }
 
-
-
-boilWater();
-console.log('chopping carrots');
+function changeColor(el,color,time) {
+ const promise = new Promise((resolve, reject) => {
+  if (el) {
+   setTimeout(() => {
+    el.style.color = color;
+    resolve();
+   }, time);
+  } else {
+   reject(new Error('failed to load image from the url'));
+  }
+ })
+ return promise;
+}
