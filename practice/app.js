916f47abe180550d51c8217c41afa5c54558e2b0
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2022-12-15 19:27:23 -0600

    asynchronous javascript

--- practice/app.js
+++ practice/app.js
@@ -1,12 +1,27 @@
-import {people,container,btn} from './utilities/utils.js'
-const showPeople = () => {
- const newPeople = people.map((person) => {
-  const { name, job } = person;
-  return `<p>${name}, <strong> ${job} </strong> </p>`
- }).join('');
- container.innerHTML = newPeople;
-};
+// import {people,container,btn} from './utilities/utils.js'
+// const showPeople = () => {
+//  const newPeople = people.map((person) => {
+//   const { name, job } = person;
+//   return `<p>${name}, <strong> ${job} </strong> </p>`
+//  }).join('');
+//  container.innerHTML = newPeople;
+// };
 
-btn.addEventListener('click', () => {
- showPeople();
-});
+// btn.addEventListener('click', () => {
+//  showPeople();
+// });
+
+function boilWater() {
+ console.log('boiling water.....')
+ setTimeout(() => {
+  setTimeout(() => {
+   console.log('chopping carrots');
+  },2000)
+  console.log('boiling water is done');
+ },5000)
+}
+
+
+
+boilWater();
+console.log('chopping carrots');
