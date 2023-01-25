a5fde09bc77384ae4c6032bdf713b92397415dd4
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-01-25 02:12:37 -0600

    js revise complete

--- practice/app.js
+++ practice/app.js
@@ -60,3 +60,60 @@ let helen = account('helen', 200);
 helen.balance();
 helen.deposit(50);
 helen.withdraw(100)
+
+// tagged template literals
+const author = "Luther Mark";
+const statement = "Life is a battle";
+const negative = "not";
+const boolean = "true";
+
+const quote = color `here is the statement : "${statement}", by ${author} and it could ${negative} be more ${boolean}`;
+console.log(quote);
+
+let result = document.querySelector("#result");
+result.innerHTML = quote;
+
+function color(text, ...vars) {
+ let finalText = text.map((item,index) => {
+  return `item <strong class="blue"> ${vars[index] || ""} </strong>`;
+ })
+
+ return finalText.join("")
+}
+
+// spread operators
+const boys = ['tomy', 'johny', 'peter'];
+const girls = ['hina', 'mina', 'shina','tina'];
+const friends = [...boys, ...girls];
+console.log(friends);
+
+// rest operator
+const [tomy, johny, peter, hina, ...rest] = friends;
+console.log(tomy, johny, peter, hina, ...rest)
+
+// find, findIndex, every, some
+let grades = ['A', 'B', 'A+', 'B'];
+let allGoodGrades = grades.every((grade) => grade !== 'c');
+console.log(allGoodGrades)
+
+// object.keys(), object.values(), object.entries()
+const person = {
+ name: 'john',
+ age: 25,
+ status: 'married',
+ profession: 'developer',
+}
+const keys = Object.keys(person);
+console.log(keys)
+const values = Object.values(person);
+console.log(values)
+const entries = Object.entries(person);
+console.log(entries)
+const newEntries = entries.map(entry => {
+ const [first, second] = entry;
+ console.log(first, second);
+})
+
+
+
+
