eb8cdeb327ccfe11f4527d9605c4d2e59765f70c
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-02-14 03:28:50 -0600

    xhr json

--- practice/app.js
+++ practice/app.js
@@ -1,18 +1,24 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', '/api/api.txt')
-xhr.onreadystatechange = function () {
- if (xhr.readyState === 4 && xhr.status === 200) {
-  const resultTxt = xhr.responseText;
-  let paragraph = document.createElement('p');
-  paragraph.innerHTML = resultTxt;
-  document.body.appendChild(paragraph);
+const btn = document.querySelector('.btn');
+const url = '/api/json.json'
+btn.addEventListener('click', function () {
+ const xhr = new XMLHttpRequest();
+ xhr.open('GET', url);
+ xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+   const resultTxt = xhr.responseText;
+   let paragraph = document.createElement('p');
+   paragraph.innerHTML = resultTxt;
+   document.body.appendChild(paragraph);
+  }
+  else {
+   console.log({
+    status: xhr.status,
+    text: xhr.statusText
+   })
+  }
  }
- else {
-  console.log({
-   status: xhr.status,
-   text: xhr.statusText
-  })
- }
-}
-xhr.send();
-console.log(xhr)
+ xhr.send();
+})
+
+
+// console.log(xhr)
