0eaf820a7cfb88cd82ccc7ca9a96aa8d7c6a2b6e
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-03-03 04:37:38 -0600

    random user API project completed

--- RANDOM-USER-API/app.js
+++ RANDOM-USER-API/app.js
@@ -18,8 +18,49 @@ const fetchUser = async() => {
  }
 }
 
-const displayUser = (list) => {
- // img, name, 
+const displayUser = (person) => {
+ // img, name, email, age, street, phone, password
+const root = person.results[0]
+ const { email, phone } = root;
+ const {large:image} = root.picture;
+ const {first, last } = root.name;
+ const name = `${first} ${last}`;
+ const { age } = root.dob;
+ const { password } = root.login;
+ const { name:streetName, number } = root.location.street;
+ const street = `${streetName} ${number}`
+
+
+ img.src = image;
+ title.textContent=`My name is`
+ value.textContent = name;
+
+// first remove active class from all btns, because when you click random user button, there will be multiple icons with the active class. so first remove active class from all, and then add active class to the name icon that means icons[0]
+ icons.forEach((btn) => { btn.classList.remove('active') });
+ icons[0].classList.add('active');
+
+
+ // info object
+ const info =
+ {
+  image,
+  name,
+  email,
+  age,
+  street,
+  phone,
+  password
+ }
+ // iterate over icons
+ icons.forEach((btn) => {
+  const label = btn.dataset.label;
+  btn.addEventListener('click', () => {
+   value.textContent = info[label];
+   title.textContent = `My ${label} is`;
+   icons.forEach((btn) => { btn.classList.remove('active') });
+   btn.classList.add('active');
+  })
+ })
 }
 
 window.addEventListener('DOMContentLoaded', fetchUser);
