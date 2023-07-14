ccc322111c5537276fb786106d4476e7bb0a6d6c
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-07-14 09:51:13 -0400

    revisiting js

--- practice/app.js
+++ practice/app.js
@@ -1,11 +1,13 @@
-const url = 'https://icanhazdadjoke.com'
-const btn = document.querySelector('.btn')
-const li = document.querySelector('#list')
-btn.addEventListener('click', async () => {
-  const config = { headers: { Accept: 'application/json' } }
-  const resp = await axios.get(url, config)
-  const joke = resp.data.joke
-  const singleLi = document.createElement('li')
-  singleLi.append(joke)
-  li.append(singleLi)
+const url = 'https://api.tvmaze.com/search/shows?q='
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
+const section = document.querySelector('.image')
+form.addEventListener('submit', async function (e) {
+  e.preventDefault()
+  const value = input.value
+  const resp = await axios.get(`${url}${value}`)
+  // const data = resp.data
+  const img = document.createElement('IMG')
+  img.src = resp.data[0].show.image.medium
+  document.body.append(img)
 })
