2034a31c406ed07bc3e15b3d3efef5045ae1c0d3
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-07-20 03:46:49 -0400

    web server tuts

--- practice/app.js
+++ practice/app.js
@@ -1,39 +1,5 @@
-const fsPromises = require('fs').promises
-const path = require('path')
+// when we install node js, we also install npm. go to npmjs.com and you will get official docs and files about npm. npm packages are already developed by third party (other developers). sometimes, rather than developing something from scratch , it's very useful to use some other npm packages developed by others. EX: axios is npm package.
 
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile('./files/lorem.txt', 'utf8')
-    await fsPromises.writeFile(
-      path.join(__dirname, 'files', 'promiseWrite.txt'),
-      data
-    )
-
-    // for deleting a file
-    await fsPromises.unlink(path.join(__dirname, 'files', 'promiseWrite.txt'))
-
-    await fsPromises.appendFile(
-      path.join(__dirname, 'files', 'promiseWrite.txt'),
-      '\n\nNice to meet you!!'
-    )
-    await fsPromises.rename(
-      path.join(__dirname, 'files', 'promiseWrite.txt'),
-      path.join(__dirname, 'files', 'promiseRenamed.txt')
-    )
-
-    const newData = await fsPromises.readFile(
-      path.join(__dirname, 'files', 'promiseRenamed.txt'),
-      'utf8'
-    )
-    console.log(newData)
-  } catch (err) {
-    console.log(err)
-  }
-}
-fileOps()
-
-// exit on uncaught errors
-process.on('uncaughtException', (err) => {
-  console.error(`There is an uncaught error: ${err}`)
-  process.exit(1)
-})
+// install nodemon globally. this is a grate dev npm package that monitors our files and as we save file it automatically restarts the server. we dont need to type node fileName in the REPL .. it will do that for us. to exit nodemon type cntrl+c
+console.log('testing file....')
+console.log('more testing')
