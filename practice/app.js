87ffb9c68427597c72123de65c3b24b9d509ba01
HelenMunny <66639204+HelenMunny@users.noreply.github.com>
2023-07-21 03:41:04 -0400

    creating server using node js

--- practice/app.js
+++ practice/app.js
@@ -1,5 +1,16 @@
-// when we install node js, we also install npm. go to npmjs.com and you will get official docs and files about npm. npm packages are already developed by third party (other developers). sometimes, rather than developing something from scratch , it's very useful to use some other npm packages developed by others. EX: axios is npm package.
+const logEvents = require('./logEvents')
 
-// install nodemon globally. this is a grate dev npm package that monitors our files and as we save file it automatically restarts the server. we dont need to type node fileName in the REPL .. it will do that for us. to exit nodemon type cntrl+c
-console.log('testing file....')
-console.log('more testing')
+const EventEmitter = require('events')
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter()
+
+myEmitter.on('log', (msg) => {
+  logEvents(msg)
+})
+
+setTimeout(() => {
+  myEmitter.emit('log', 'log event emitted!')
+}, 3000)
